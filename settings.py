bc66d07d5dee80a3902d2987bcba77bbf1970bdc
hisham waleed karam <hisham.karam@cartologic.com>
2018-10-03 20:05:48 +0200

    fix settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,5 @@
-import os
-import sys
-import cartoview_basic_viewer
-current_folder = os.path.dirname(cartoview_basic_viewer.__file__)
-TEMPLATES[0]['DIRS'] = [os.path.join(
-    current_folder, 'templates')]+TEMPLATES[0]['DIRS']
-TEMPLATES[0]["OPTIONS"]['context_processors'] += (
-    'cartoview_basic_viewer.context_processors.geocoding_processor',)
+# import os
+# import sys
+# import cartoview_basic_viewer
+# current_folder = os.path.dirname(cartoview_basic_viewer.__file__)
 # OPENCAGE_KEY = 'YourKey'
